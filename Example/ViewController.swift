e82108faed1da50859223c0cf85f835b5ab7bcb9
Jessica Cavazos <jessycavazos95@gmail.com>
2016-06-24 14:16:19 -0700

    Added Name

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Hello")
+        
+        var name = "Jessica"
+        
+        name += "Cavazos"
     }
 
     override func didReceiveMemoryWarning() {
